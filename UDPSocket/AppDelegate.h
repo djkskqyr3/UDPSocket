564c76e98959d8e80f5bae17ac4e557ff461b234
JinJoUn <jameschan0524@gmail.com>
2015-01-23 00:05:55 +0800

    Initial Commit

---
+++ UDPSocket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UDPSocket
+//
+//  Created by JinJoUn on 1/23/15.
+//  Copyright (c) 2015 JamesChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UDPSocket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UDPSocket
+//
+//  Created by JinJoUn on 1/23/15.
+//  Copyright (c) 2015 JamesChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
